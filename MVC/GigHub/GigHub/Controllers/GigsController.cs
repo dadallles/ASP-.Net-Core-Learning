735085f9faa7dd6733cb662e74e64e1120a202ee
Dawid <dadales@o2.pl>
2020-04-17 12:21:12 +0200

    chapters: 1-8

---
+++ MVC/GigHub/GigHub/Controllers/GigsController.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using GigHub.Data;
+using GigHub.ViewModels;
+using GigHub.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            /*
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            var artistId = claim.Value;
+            */
+
+            /*
+            var artist = _context.Users.Single(u => u.Id == artistId);
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+            */
+
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("Create", viewModel);
+            }
+
+            var gig = new Gig
+            {
+                ArtistId = User.FindFirstValue(ClaimTypes.NameIdentifier),
+                DateTime = viewModel.GetDateTime(),
+                GenreID = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ MVC/GigHub/GigHub/Data/Migrations/20200416133044_GigAddForeignKey.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GigHub.Data.Migrations
+{
+    public partial class GigAddForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Gigs_Genres_GenreId",
+                table: "Gigs");
+
+            migrationBuilder.RenameColumn(
+                name: "GenreId",
+                table: "Gigs",
+                newName: "GenreID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Gigs_GenreId",
+                table: "Gigs",
+                newName: "IX_Gigs_GenreID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Gigs_Genres_GenreID",
+                table: "Gigs",
+                column: "GenreID",
+                principalTable: "Genres",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Gigs_Genres_GenreID",
+                table: "Gigs");
+
+            migrationBuilder.RenameColumn(
+                name: "GenreID",
+                table: "Gigs",
+                newName: "GenreId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Gigs_GenreID",
+                table: "Gigs",
+                newName: "IX_Gigs_GenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Gigs_Genres_GenreId",
+                table: "Gigs",
+                column: "GenreId",
+                principalTable: "Genres",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

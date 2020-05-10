74c27b8e2953e1cfe9953ca6f424ad1add5b7fc7
Dawid <dadales@o2.pl>
2020-05-10 20:47:16 +0200

    start

---
+++ Evento API/src/Evento.Core/Domain/Event.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evento.Core.Domain
+{
+    public class Event : Entity
+    {
+        private ISet<Ticket> _tickets = new HashSet<Ticket>();
+
+        public string Name { get; protected set; }
+
+        public string Description { get; protected set; }
+
+        public DateTime CreatedAt { get; protected set; }
+
+        public DateTime StartDate { get; protected set; }
+
+        public DateTime EndDate { get; protected set; }
+
+        public DateTime UpdateAt { get; protected set; }
+
+        public IEnumerable<Ticket> Tickets => _tickets;
+
+
+        protected Event()
+        {
+
+        }
+
+        public Event(Guid id, string name, string description, DateTime startDate, DateTime endDate)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            StartDate = startDate;
+            EndDate = endDate;
+            CreatedAt = DateTime.UtcNow;
+            UpdateAt = DateTime.UtcNow;
+        }
+
+        public void AddTickets(int amount, decimal price)
+        {
+            for(int i=0; i<amount; ++i)
+            {
+                var seating = _tickets.Count + 1;
+
+                _tickets.Add(new Ticket(this, seating, price));
+            }
+        }
+    }
+}
